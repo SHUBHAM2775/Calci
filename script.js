f490172ff521eab83fb2b748abc601d1bf89a4a3
SHUBHAM2775 <shubhamu1332@gmail.com>
2024-11-15 20:04:07 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById('display');
+
+function calculate(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function result(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(err){
+        display.value = "Error";
+    }
+}
